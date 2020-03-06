3343c21e015c56a6317ddba63417c27eed2ab0c6
DouglasGo8 <sgtear@hotmail.com>
2020-03-05 22:58:47 -0300

    Threads in running

--- app-concurrency-deep-dive/src/test/java/com/packtpub/java/coding/problems/concurrency/deepdive/App.java
+++ app-concurrency-deep-dive/src/test/java/com/packtpub/java/coding/problems/concurrency/deepdive/App.java
@@ -1,12 +1,13 @@
 package com.packtpub.java.coding.problems.concurrency.deepdive;
 
+import lombok.Setter;
 import lombok.SneakyThrows;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+import java.util.function.Function;
 
 
 import static java.lang.System.out;
@@ -148,5 +149,92 @@ public class App {
         }).get();
     }
 
+    @Test
+    @SneakyThrows
+    public void combiningMultipleFuture() {
+
+        final Function<String, CompletableFuture<String>> f1 = (n) -> CompletableFuture.supplyAsync(() -> "Hi ".concat(n));
+        final Function<String, CompletableFuture<String>> f2 = (n) -> CompletableFuture.supplyAsync(() -> n.concat(" well done.."));
+
+        var result = f1.apply("Cargo").thenCompose(f2).get();
+
+        out.println(result);
+
+    }
+
+    @Test
+    public void optimizeBusyWait() throws InterruptedException {
+
+        final class StartService implements Runnable {
+
+            @Setter
+            private volatile boolean isServiceAvailable;
+
+            @Override
+            public void run() {
+
+                while (!isServiceAvailable) {
+                    // Use spin-wait hit (ask to processor to optimize the resource)
+                    // This should perform better if the underling hardware support
+                    // the hint
+                    Thread.onSpinWait();
+                }
+                serviceRun();
+            }
+
+            private void serviceRun() {
+                out.println("Service is running...");
+            }
+
+        }
+
+        var startService = new StartService();
+        new Thread(startService).start();
+        Thread.sleep(5000);
+
+        startService.setServiceAvailable(true);
+
+
+    }
+
+    @Test
+    public void taskCancellation() throws InterruptedException {
+
+        final class RandomList implements Runnable {
+
+            private volatile boolean isCancelled;
+            private Random random = new Random();
+            private final List<Integer> randoms = new CopyOnWriteArrayList<>();
+
+            @Override
+            public void run() {
+                while (!isCancelled) {
+                    randoms.add(random.nextInt(100));
+                }
+            }
+
+            public void cancel() {
+                isCancelled = true;
+            }
+
+            public List<Integer> getRandoms() {
+                return randoms;
+            }
+
+        }
+
+        var randomList = new RandomList();
+        var executor = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < 100; i++) {
+            executor.execute(randomList);
+        }
+        Thread.sleep(100);
+        randomList.cancel();
+        randomList.getRandoms().forEach(out::println);
+    }
+
+
+    
 
 }
